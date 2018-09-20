96df1f7ce9117a5707a302a0e175106962f557f6
Cameron94857 <beillard21@up.edu>
2018-09-19 18:01:41 -0700

    -Edge detection filter added -Smoothing and edge detection merged and fixed

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
@@ -23,11 +23,11 @@ public class BrightnessFilter extends PhotoFilter {
     * @param inPixel is a 32 bit pixel that contains RGB color values
     * @return a new Pixel in which each of the RGB components has been increased
     */
-    public int transformPixel(int inPixel) {
-        int red = constrain(Color.red(inPixel) + ADJUSTMENT);
-        int green = constrain(Color.green(inPixel) + ADJUSTMENT);
-        int blue = constrain(Color.blue(inPixel) + ADJUSTMENT);
-        int outPixel = Color.argb(Color.alpha(inPixel), red, green, blue);
+    public int transformPixel(int p0, int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8) {
+        int red = constrain(Color.red(p0) + Color.red(p1) - Color.red(p2) + Color.red(p3) - 2*Color.red(p4) - Color.red(p5) + Color.red(p6) + Color.red(p7) - Color.red(p8));
+        int green = constrain(Color.green(p0) + Color.green(p1) - Color.green(p2) + Color.green(p3) - 2*Color.green(p4) - Color.green(p5) + Color.green(p6) + Color.green(p7) - Color.green(p8));
+        int blue = constrain(Color.blue(p0) + Color.blue(p1) - Color.blue(p2) + Color.blue(p3) - 2*Color.blue(p4) - Color.blue(p5) + Color.blue(p6) + Color.blue(p7) - Color.blue(p8));
+        int outPixel = Color.argb(Color.alpha(p4), red, green, blue);
         return outPixel;
     }
 
--- app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFilter.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFilter.java
@@ -72,4 +72,5 @@ public abstract class PhotoFilter {
         }
         return newBmp;
     }
+
 }
