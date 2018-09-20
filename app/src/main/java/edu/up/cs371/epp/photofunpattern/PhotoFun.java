1c30831fd6cdedaf19f1fa146afaa2e87c94abad
Cameron94857 <beillard21@up.edu>
2018-09-19 19:38:43 -0700

    -Worked on image selection spinner component code -All that needs to be done is finish the onItemSelected method in the drawablePickerClicked class

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.graphics.Bitmap;
         import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
         import android.widget.Button;
         import android.view.View;
 import android.widget.Spinner;
+
+import java.util.logging.Logger;
 
 /**
  *  class PhotoFun controls this photo manipulation app.
@@ -22,6 +26,8 @@ import android.widget.Spinner;
 public class PhotoFun extends AppCompatActivity {
 
     // Image resources
+    private ImageView originalImageView;
+    private BitmapDrawable originalDrawableBmp;
     private Bitmap myOriginalBmp;
     private ImageView myNewImageView;
     private Spinner drawablePicker;
@@ -37,10 +43,10 @@ public class PhotoFun extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_fun);
 
-        ImageView originalImageView =
+        originalImageView =
                 (ImageView) findViewById(R.id.originalImage);
-        BitmapDrawable originalDrawableBmp =
-                (BitmapDrawable) originalImageView.getDrawable();
+        originalDrawableBmp =
+               (BitmapDrawable) originalImageView.getDrawable();
         myOriginalBmp = originalDrawableBmp.getBitmap();
 
         myNewImageView = (ImageView) findViewById(R.id.newImage);
@@ -52,9 +58,39 @@ public class PhotoFun extends AppCompatActivity {
                 (Button) findViewById(R.id.brightnessFilterButton);
         brightnessFilterButton.setOnClickListener
                 (new brightnessFilterButtonListener());
+
         drawablePicker = (Spinner) findViewById(R.id.drawableSpinner);
+        ArrayAdapter<CharSequence> itemsAdapter = ArrayAdapter.createFromResource(this, R.array.drawables_array, android.R.layout.simple_spinner_item);
+        itemsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        drawablePicker.setAdapter(itemsAdapter);
+        drawablePicker.setOnItemSelectedListener(new drawablePickerClicked());
+
     }
 
+    private class drawablePickerClicked implements AdapterView.OnItemSelectedListener{
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+           if (parent.getItemAtPosition(pos).toString() == "cheese"){
+               //finish BitmapDrawable constructors
+               originalDrawableBmp = new BitmapDrawable();
+           }
+           else if(parent.getItemAtPosition(pos).toString() == "edcepp"){
+
+           }
+           else if(parent.getItemAtPosition(pos).toString() == "two"){
+
+           }
+           else{
+
+           }
+           originalImageView.setImageBitmap(originalDrawableBmp.getBitmap());
+        }
+
+        public void onNothingSelected(AdapterView<?> parent){
+
+        }
+
+    }
     /*
     * class grayFilterButtonListener this inner class defines the action for
     * the gray filter button.
@@ -74,7 +110,7 @@ public class PhotoFun extends AppCompatActivity {
     private class brightnessFilterButtonListener
             implements View.OnClickListener {
         public void onClick(View button) {
-            BrightnessFilter filter = new BrightnessFilter();
+            EdgeDetectionFilter filter = new EdgeDetectionFilter();
             myNewImageView.setImageBitmap(filter.apply(myOriginalBmp));
         }
     }
